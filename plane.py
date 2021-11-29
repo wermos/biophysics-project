7542d85248ca5112fabc58dbb71e46fe212aa1ed
Divyansh Tiwari <200020049@iitb.ac.in>
2021-11-29 16:57:08 +0530

    added comments and subplots

--- plane.py
+++ plane.py
@@ -6,12 +6,12 @@ from scipy.integrate import odeint
  
 
 N=10000 # no. of steps
-eta2D=1
+eta2D=1 # values of parameters
 k=1    
 
 
 
-def vx_dipole(x,y,alpha,x0,y0,alpha0):
+def vx_dipole(x,y,alpha,x0,y0,alpha0):    # calculating the x-component of the dipole
     r= sqrt((x-x0)**2 +(y-y0)**2)
     b= -k/(4*pi*eta2D*r)
     a1 = (x-x0)/r
@@ -20,7 +20,7 @@ def vx_dipole(x,y,alpha,x0,y0,alpha0):
     return f
 
 
-def vy_dipole(x,y,alpha,x0,y0,alpha0):
+def vy_dipole(x,y,alpha,x0,y0,alpha0):    # calculating the y-component of the dipole
     r= sqrt((x-x0)**2+(y-y0)**2 )
     b= -k/(4*pi*eta2D*r)
     a1 = (x-x0)/r
@@ -28,7 +28,7 @@ def vy_dipole(x,y,alpha,x0,y0,alpha0):
     f=b*(1-2*(a1*math.cos(alpha0) + a2*math.sin(alpha0))**2)*a2
     return f
 
-def planerMotion(y,time):
+def planerMotion(y,time):                           # model function used to calculate the derivative of y
     
     g0=vx_dipole(y[0],y[1],y[2],y[3],y[4],y[5])
     g1=vy_dipole(y[0],y[1],y[2],y[3],y[4],y[5])
@@ -41,32 +41,58 @@ def planerMotion(y,time):
     return array([g0,g1,g2,g3,g4,g5])
 
 
-y=zeros([6]) # initial value
-x1=y1=alpha1=0#initial value of particle 1
-x1=0
-
-
+y=zeros([6])   # y stores the initial values values of the variables.
+x1=y1=alpha1=0 #initial value of particle 1
 x2=0.4
 y2=0
 alpha2=pi/6
 
 time =linspace(0,200,N)
-y[0]=x1
-y[1]=y1
-y[2]=alpha1
-y[3]=x2
-y[4]=y2
-y[5]=alpha2
+y[0]=x1     # x coordinate of particle-1
+y[1]=y1     # y coordinate of particle-1
+y[2]=alpha1 # angle of rotation of  particle-1
+y[3]=x2     # x coordinate of particle-2
+y[4]=y2     # y coordinate of particle-2
+y[5]=alpha2 # angle of rotation of  particle-2
+ 
 
+answer = odeint(planerMotion,y,time)   # storing the answer using odeint method of scipy lib
 
-answer = odeint(planerMotion,y,time,atol=1e-7, rtol=1e-11, mxstep=5000)
-xdata= answer[:,1]
+xdata= answer[:,0]   #info and plots on x-coordinates
+ydata =answer[:,3]
+
+plt.subplot(1,3,1)
+plt.plot(time,xdata,color='r',label='particle1')
+plt.plot(time,ydata,color='g',label='particle2')
+
+plt.xlabel('time')
+plt.ylabel('x-coordinate')
+plt.legend(['particle 1','particle 2'])
+
+xdata= answer[:,1]   #info and plots on y-coordinates
 ydata =answer[:,4]
+
+plt.subplot(1,3,2)
 plt.plot(time,xdata,color='r',label='particle1')
 plt.plot(time,ydata,color='g',label='particle2')
 
 plt.xlabel('time')
 plt.ylabel('y-coordinate')
 plt.legend(['particle 1','particle 2'])
+
+xdata= answer[:,2]   #info and plots on aplpha
+ydata =answer[:,5]
+
+plt.subplot(1,3,3)
+plt.plot(time,xdata,color='r',label='particle1')
+plt.plot(time,ydata,color='g',label='particle2')
+
+plt.xlabel('time')
+plt.ylabel('Alpha')
+plt.legend(['particle 1','particle 2'])
+
+
+
 plt.show()
+
 
